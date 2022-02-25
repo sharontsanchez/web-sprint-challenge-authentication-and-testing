533296011c31ed34429e9ff93c62856d74e58bd3
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-25 14:56:19 -0600

    restricted middleware set up

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,7 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
+const { JWT_SECRET } = require("../../config");
+const jwt = require('jsonwebtoken')
+
+ /*
     IMPLEMENT
 
     1- On valid token in the Authorization header, call next.
@@ -11,4 +12,18 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: "token required" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next({ status: 401, message: "token invalid" });
+    }
+    req.decodedJwt = decoded;
+    next();
+  });
+ 
 };
