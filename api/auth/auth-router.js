8ccf2be6be9293d233addb1e97bb33ff5db0f677
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-25 14:32:47 -0600

    set up auth-middleware for auth-router and connected

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
+const { validateRegistrationBody, checkUsernameAvailable } = require("./auth-middleware")
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -27,10 +26,12 @@ router.post('/register', (req, res) => {
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
+
+router.post('/register', validateRegistrationBody, checkUsernameAvailable, (req, res) => {
+  res.end('implement register, please!');
+
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -54,6 +55,11 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
+
+
+router.post('/login', (req, res) => {
+  res.end('implement login, please!');
+
 });
 
 module.exports = router;
